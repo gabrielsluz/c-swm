1161721e6f570c725f26b83ac825f921d1ce8cca
gabrielsluz <gabrielsluz1999@gmail.com>
2020-10-23 11:30:53 -0300

    Seed update

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -3,7 +3,7 @@
 #Train10 => eval_all => eval_all_use_trans_model => eval 1 => eval 5 => eval 10
 #Train20 => eval_all => eval_all_use_trans_model => eval 1 => eval 5 => eval 10
 
-for seed in 1 2 3 ; do
+for seed in 4 5 6 ; do
   for num_epochs in 5 15 25; do
     python3 train.py --seed $seed --dataset /datasets/c_swm_data/mmnist_train.h5 --encoder large --embedding-dim 10 --num-objects 15 --epochs $num_epochs --name mmnist --ignore-action --batch-size 512
     python3 eval_all.py --padded_mnist_path /datasets/c_swm_data/padded_mnist.npz --save-folder checkpoints/mmnist/ --results-file eval_all_results.txt
