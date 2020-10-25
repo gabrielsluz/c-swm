71aa80967b3297643d0113e7de91b7dc38880834
gabrielsluz <gabrielsluz1999@gmail.com>
2020-10-24 23:50:29 -0300

    model re define

--- train_eval_fine_tune.py
+++ train_eval_fine_tune.py
@@ -96,20 +96,6 @@ def train_and_eval(args, eval_every, use_trans_model, ft_data_loader, ft_eval_da
         temperature=args.temperature).to(device)
 
     model.apply(utils.weights_init)
-    #Copy model for fine tuning
-    model_clone = modules.ContrastiveSWM(
-        embedding_dim=args.embedding_dim,
-        hidden_dim=args.hidden_dim,
-        action_dim=args.action_dim,
-        input_dims=input_shape,
-        num_objects=args.num_objects,
-        sigma=args.sigma,
-        hinge=args.hinge,
-        ignore_action=args.ignore_action,
-        copy_action=args.copy_action,
-        encoder=args.encoder,
-        use_nt_xent_loss=args.use_nt_xent_loss,
-        temperature=args.temperature).to('cpu')
 
     optimizer = torch.optim.Adam(
         model.parameters(),
@@ -159,6 +145,20 @@ def train_and_eval(args, eval_every, use_trans_model, ft_data_loader, ft_eval_da
             torch.save(model.state_dict(), model_file)
         
         if epoch % eval_every == 0 or epoch == args.epochs:
+            #Copy model for fine tuning
+            model_clone = modules.ContrastiveSWM(
+                embedding_dim=args.embedding_dim,
+                hidden_dim=args.hidden_dim,
+                action_dim=args.action_dim,
+                input_dims=input_shape,
+                num_objects=args.num_objects,
+                sigma=args.sigma,
+                hinge=args.hinge,
+                ignore_action=args.ignore_action,
+                copy_action=args.copy_action,
+                encoder=args.encoder,
+                use_nt_xent_loss=args.use_nt_xent_loss,
+                temperature=args.temperature).to('cpu')
             model_clone.load_state_dict(copy.deepcopy(model.state_dict())) #Deepcopy does not work on model
             model_clone.to(device)
             ft_acc_list = fine_tune_and_eval_downstream(model_clone, device, ft_data_loader, ft_eval_data_loader,
